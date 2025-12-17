8cb98d87629ab42b0dfe2b9a09a15b05fa4d0147
Boghul <82718422+Boghul@users.noreply.github.com>
2025-12-17 16:16:35 +0100

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,284 @@
+#Not finished yet, still in work!
+
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+ 
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "deadacute";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+  # Hardware Configuration #Steam #Controller #OpenGL #fwupd #Bluetooth
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        rocmPackages.clr.icd # OpenCL
+        libva # VA-API
+      ];
+      extraPackages32 = [ ];
+    };
+    amdgpu = {
+      initrd.enable = true;
+      opencl.enable = true;
+      overdrive.enable = true;
+    };
+    steam-hardware.enable = true;
+    #xone.enable = true;
+    #xpadneo.enable = true;
+    cpu.amd.updateMicrocode = true;
+
+bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      # Shows battery charge of connected devices on supported
+      # Bluetooth adapters. Defaults to 'false'.
+      Experimental = true;
+      # When enabled other devices can connect faster to us, however
+      # the tradeoff is increased power consumption. Defaults to
+      # 'false'.
+      FastConnectable = true;
+    };
+    Policy = {
+      # Enable all controllers when they are found. This includes
+      # adapters present on start as well as adapters that are plugged
+      # in later on. Defaults to 'true'.
+      AutoEnable = true;
+     };
+   };
+ };
+};
+hardware.enableAllFirmware = true;
+
+ 
+
+
+
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.xenomorph = {
+    isNormalUser = true;
+    description = "Thorsten ";
+    extraGroups = [ "networkmanager"
+		    "wheel"
+	  	    "audio"
+		    "gamemode"
+       		    "cpugovctl"
+       		    "sysctlgroup"
+       		    "libvirtd"
+       		    "kvm"
+       		    "input"
+       		    "plugdev"
+       		    "tss"
+       		    "immich"
+                  ];
+    packages = with pkgs; [
+      kdePackages.kate
+      thunderbird
+      zapzap
+     
+  ];
+};
+
+ 
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Install niri.
+  programs.niri.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Install openrgb
+  services.hardware.openrgb.enable = true;
+
+  # Gui BluetoothManager
+  services.blueman.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  #$ nix search wget
+  environment.systemPackages = with pkgs; [
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  pkgs.fastfetch
+  kitty
+  ninja
+  cmake
+  git
+  openrgb-with-all-plugins
+  pkgs.vicinae
+  fuzzel
+  musikcube
+  lutris
+  zsh
+  oh-my-zsh
+  swaybg
+  htop
+  ranger
+  qbittorrent
+  mpv
+  steam
+  steam-run
+ protonup-qt
+  xorg.libXcursor
+    xorg.libXi
+    xorg.libXinerama
+    xorg.libXScrnSaver
+    libpng
+    libpulseaudio
+    libvorbis
+    stdenv.cc.cc.lib # Provides libstdc++.so.6
+    libkrb5
+    keyutils
+  
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+# Activate flakes install
+nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+# Shell set up
+# for global user
+users.defaultUserShell=pkgs.zsh; 
+# For a specific user
+users.users.xenomorph.shell = pkgs.zsh; 
+users.users.xenomorph.useDefaultShell = true;
+programs.zsh.enable = true;
+# enable zsh and oh my zsh
+
+programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+};
+
+programs.xwayland.enable = true;
+
+services = {  
+
+    flatpak = {
+      enable = true;
+    };
+    fwupd.enable = true;
+    spice-vdagentd.enable = true;
+    spice-webdavd.enable = true;
+    resolved.enable = true;
+
+};
+
+
+
+
+
+
+}

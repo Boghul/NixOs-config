53e61f45e41ef32a8325cea7624a479cce3a256c
Boghul <82718422+Boghul@users.noreply.github.com>
2025-12-20 19:51:54 +0100

    Update flake.nix
    added solaar flake

--- flake.nix
+++ flake.nix
@@ -5,7 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     vicinae.url = "github:vicinaehq/vicinae";
+
     awww.url = "git+https://codeberg.org/LGFae/awww";
+    
+    solaar = {
+      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
+      #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.6.tar.gz"; # uncomment line for solaar version 1.1.18
+      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     
     steam-fetcher.url = "github:aidalgol/nix-steam-fetcher";  # Steam fetcher for NixOS
@@ -22,16 +30,17 @@
                       vicinae, 
                       awww, 
                       home-manager,
-                      steam-fetcher }: {
-    specialArgs = { inherit inputs; };
-
+                      steam-fetcher,
+                      solaar  }:   {
+   
+        specialArgs = { inherit inputs; };
     
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         modules = [ ./configuration.nix
                     ./hardware-configuration.nix
-                  # ./vicinae
-                  #  ./awww.nix
                   
+                                     
+                  solaar.nixosModules.default
                   home-manager.nixosModules.home-manager 
                    {
           home-manager = {

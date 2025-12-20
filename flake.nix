542701b74ae960dccd81e705fc5bc1aa4f07cb22
Boghul <82718422+Boghul@users.noreply.github.com>
2025-12-20 11:29:25 +0100

    Update flake.nix
    added home manager

--- flake.nix
+++ flake.nix
@@ -3,16 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-     #  nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  };
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    vicinae.url = "github:vicinaehq/vicinae";
+    awww.url = "git+https://codeberg.org/LGFae/awww";
 
-  outputs = { self, nixpkgs }: {
+    
+    steam-fetcher.url = "github:aidalgol/nix-steam-fetcher";  # Steam fetcher for NixOS
 
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+       home-manager = {
+        url = "github:nix-community/home-manager/release-25.05";
+        inputs.nixpkgs.follows = "nixpkgs";
+        };
+   };
+
+   outputs = inputs@{ self,
+                      nixpkgs,
+                      systems, 
+                      vicinae, 
+                      awww, 
+                      home-manager,
+                      steam-fetcher }: {
+    specialArgs = { inherit inputs; };
+
+    
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         modules = [ ./configuration.nix
                     ./hardware-configuration.nix
-                                         ];
-        };
-
+                  # ./vicinae
+                  #  ./awww.nix
+                  
+                  home-manager.nixosModules.home-manager 
+                   {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.xenomorph = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
   };
 }

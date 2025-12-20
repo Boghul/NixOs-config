9601082990387e6a6bfe188573e2af6c884122bc
Boghul <82718422+Boghul@users.noreply.github.com>
2025-12-20 19:57:26 +0100

    Update configuration.nix
    added solaar flake,
    added service for mx master 3s mouse,
    added clamav,
    added proton bridge,
    added heroic game launcher,
    added steam

--- configuration.nix
+++ configuration.nix
@@ -10,11 +10,9 @@ with lib;
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      #./fuzzel.nix
-      #./steam.nix
+    [ # Include the results of the hardware scan
       ./flatpak.nix
-      #./vicinae.nix
+      #./default.nix 
     ];
   # Bootloader.
   boot = {
@@ -36,6 +34,7 @@ with lib;
       "amdgpu.dc=1"
       "amd_iommu=on"
       "acpi_osi=Linux"
+      "quiet"
       ];
     kernelModules = [
       "kvm-amd"
@@ -104,13 +103,21 @@ with lib;
 
 services = {  
     flatpak = {
-      enable = true;
-      };
+        enable = true;
+        };
     fwupd.enable = true;
     spice-vdagentd.enable = true;
     spice-webdavd.enable = true;
     resolved.enable = true;
-    gnome.gnome-keyring.enable = true;  
+    gnome.gnome-keyring.enable = true;
+    clamav = {
+        daemon.enable = true;
+        updater.enable = true;
+        };
+    protonmail-bridge = {
+        enable = true;
+        logLevel = "info"; # Options: panic, fatal, error, warn, info, debug
+        };  
   # Enable CUPS to print documents.
     printing.enable = true;
   # Enable the KDE Plasma Desktop Environment.
@@ -125,6 +132,7 @@ services = {
    # displayManager.sddm.wayland.enable = true;
   # Configure keymap in X11
     xserver = {
+        displayManager.startx.enable = true;
         xkb = {
             layout = "de";
             variant = "deadacute";
@@ -156,8 +164,39 @@ services = {
   # t o find the controller and product ID of a device usbutils might be useful 
   #udev.extraRules = ''
   #  SUBSYSTEM=="input", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="3106", MODE="0660", GROUP="input"
-  #'';  
- };
+  #'';
+  };
+  solaar = {
+    enable = true; # Enable the service
+    package = pkgs.solaar; # The package to use
+    window = "hide"; # Show the window on startup (show, *hide*, only [window only])
+    batteryIcons = "regular"; # Which battery icons to use (*regular*, symbolic, solaar)
+    extraArgs = ""; # Extra arguments to pass to solaar on startup
+  };
+  # Logitech mx master 3s setup
+    udev.extraRules = ''  
+    # Allows non-root users to have raw access to Logitech devices.  
+    # Allowing users to write to the device is potentially dangerous  
+    # because they could perform firmware updates.  
+    KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"    
+    ACTION == "remove", GOTO="solaar_end"  
+    SUBSYSTEM != "hidraw", GOTO="solaar_end"    
+    # USB-connected Logitech receivers and devices  
+    ATTRS{idVendor}=="046d", GOTO="solaar_apply"    
+    # Lenovo nano receiver  
+    ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="6042", GOTO="solaar_apply"    
+    # Bluetooth-connected Logitech devices  
+    KERNELS == "0005:046D:*", GOTO="solaar_apply"    
+    GOTO="solaar_end"    
+    LABEL="solaar_apply"    
+    # Allow any seated user to access the receiver.  
+    # uaccess: modern ACL-enabled udev  
+    TAG+="uaccess"    
+    # Grant members of the "plugdev" group access to receiver (useful for SSH users)  
+    #MODE="0660", GROUP="plugdev"    
+    LABEL="solaar_end"  
+    # vim: ft=udevrules  
+    '';
 
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
@@ -175,6 +214,9 @@ services = {
       extraPackages = with pkgs; [
         rocmPackages.clr.icd # OpenCL
         libva # VA-API
+        libva-vdpau-driver
+        libvdpau-va-gl
+        mesa
         ];
       extraPackages32 = [ ];
     };
@@ -188,31 +230,35 @@ services = {
     xpadneo.enable = true;
     cpu.amd.updateMicrocode = true;
 
-bluetooth = {
-  enable = true;
-  powerOnBoot = true;
-  settings = {
-    General = {
-      # Shows battery charge of connected devices on supported
-      # Bluetooth adapters. Defaults to 'false'.
-      Experimental = true;
-      # When enabled other devices can connect faster to us, however
-      # the tradeoff is increased power consumption. Defaults to
-      # 'false'.
-      FastConnectable = true;
-    };
-    Policy = {
+    logitech.wireless.enable = true;
+    bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+        settings = {
+            General = {
+                # Shows battery charge of connected devices on supported
+                #Bluetooth adapters. Defaults to 'false'.
+                Experimental = true;
+                # When enabled other devices can connect faster to us, however
+                # the tradeoff is increased power consumption. Defaults to
+                # 'false'.
+                FastConnectable = true;
+                };
+        Policy = {
       # Enable all controllers when they are found. This includes
       # adapters present on start as well as adapters that are plugged
       # in later on. Defaults to 'true'.
-      AutoEnable = true;
-     };
-   };
- };
+            AutoEnable = true;
+            };
+        };
+    };
 };
 
  # Security
   security = {
+    polkit.enable = true;
+    protectKernelImage = true;
+    forcePageTableIsolation = true;
     rtkit.enable = true;
     tpm2 = {
       enable = true;
@@ -256,9 +302,7 @@ bluetooth = {
        		    "sysctlgroup"
        		    "libvirtd"
        		    "kvm"
-       		    "input"
-       		    "plugdev"
-       		    "tss"
+       		    "input"       		    
        		      ];
     packages = with pkgs; [ ];
    };
@@ -266,7 +310,14 @@ bluetooth = {
 
   environment = {
     sessionVariables = {
-    AMD_VULKAN_ICD = "RADV";
+    AMD_VULKAN_ICD           = "RADV";
+    PROTON_USE_NTSYNC        = "1";
+    ENABLE_HDR_WSI           = "1";
+    DXVK_HDR                 = "1";
+    PROTON_ENABLE_AMD_AGS    = "1";
+    PROTON_ENABLE_NVAPI      = "1";
+    ENABLE_GAMESCOPE_WSI     = "1";
+    STEAM_MULTIPLE_XWAYLANDS = "1";
 };
   # List packages installed in system profile. To search, run:
   #$ nix search wget
@@ -285,7 +336,7 @@ bluetooth = {
   git
   tree
   gnugrep
-
+  
   # zsh
   zsh
   oh-my-zsh
@@ -310,7 +361,7 @@ bluetooth = {
   wasistlos
   signal-desktop
   #pkgs.protonmail-desktop
-  pkgs.protonmail-bridge
+  protonmail-bridge
   thunderbird
 
   # programs
@@ -332,6 +383,7 @@ bluetooth = {
   xorg.libXi
   xorg.libXinerama
   xorg.libXScrnSaver
+  xorg.libxcb
 
   # lib
   libpng
@@ -341,6 +393,7 @@ bluetooth = {
   libkrb5
   libjxl
   libadwaita
+  libxcb
 
   # Secureboot
   sbctl
@@ -354,6 +407,7 @@ bluetooth = {
   fido2-manage
   libfido2
   gnupg
+  clamav
 
   # General utilities
   swaybg
@@ -374,6 +428,7 @@ bluetooth = {
   tpm2-tools
   tpm2-tss
   wayland
+  wayland-utils
   rar
   unrar-free
   bat
@@ -391,12 +446,16 @@ bluetooth = {
   clinfo
   solaar
   nettools
+  strace
   tealdeer
   xclip
   bat
   kdePackages.kate
+  kdePackages.ktexteditor
   xhost
   xwayland-satellite
+  xcb-util-cursor
+  clang
   (pkgs.wrapOBS {
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs
@@ -416,6 +475,10 @@ bluetooth = {
   mangohud
   gamescope
   gamemode
+  vulkan-tools
+  vulkan-loader
+  vulkan-validation-layers
+  vulkan-extension-layer
   
   # Virtualization stuff
   spice-gtk
@@ -437,10 +500,10 @@ bluetooth = {
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
+   programs.gnupg.agent = {
+     enable = true;
   #   enableSSHSupport = true;
-  # };
+   };
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
@@ -452,8 +515,9 @@ bluetooth = {
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
-# Activate flakes install
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
+nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];    
+};
 
 # Steam
 programs.steam = {
@@ -508,34 +572,37 @@ programs.steam = {
     xwayland.enable = true;
     firefox.enable = true;
     adb.enable = true;
-    gamescope.enable = true; 
+    gamescope = {
+        enable = true;
+        capSysNice = true;
+        }; 
     gamemode.enable = true;
     obs-studio.enable = true;
     nano = {
-    enable = true;
-    nanorc = "
-        set mouse
-        set autoindent
-        set linenumbers
-        set constantshow
-        set noconvert
-        set preserve
-        set smarthome
-        set tabsize 4
-        set tabstospaces
-        set wordbounds
-        set zap
-        set errorcolor brightwhite,red
-    	set functioncolor green
-    	set keycolor cyan
-    	set numbercolor cyan
-    	set selectedcolor brightwhite,magenta
-    	set statuscolor cyan
-    	set stripecolor ,yellow
-    	set titlecolor brightwhite,blue
-      ";
-    syntaxHighlight = true;
-    };
+        enable = true;
+        nanorc = "
+            set mouse
+            set autoindent
+            set linenumbers
+            set constantshow
+            set noconvert
+            set preserve
+            set smarthome
+            set tabsize 4
+            set tabstospaces
+            set wordbounds
+            set zap
+            set errorcolor brightwhite,red
+    	    set functioncolor green
+    	    set keycolor cyan
+    	    set numbercolor cyan
+    	    set selectedcolor brightwhite,magenta
+    	    set statuscolor cyan
+    	    set stripecolor ,yellow
+    	    set titlecolor brightwhite,blue
+            ";
+        syntaxHighlight = true;
+        };
     nh = {
       enable = true;
       clean = {
@@ -558,7 +625,7 @@ programs.steam = {
     zsh = {
       enable = true;
       enableCompletion = true;
-    enableBashCompletion = true;
+      enableBashCompletion = true;
       autosuggestions.enable = true;
       syntaxHighlighting = {
         enable = true;
@@ -568,12 +635,10 @@ programs.steam = {
       };
      setOptions = [
       "AUTO_CD"
-    ];
+      ];
       histSize = 10000;
-    shellAliases = {
-      #...
+      shellAliases = {  };
     };
-  };
 };
 
  # Virtualization software
@@ -601,7 +666,7 @@ programs.steam = {
      "noatime"
    ];
  };
-system.autoUpgrade = {
-    enable = true;
-    };
+    system.autoUpgrade = {
+        enable = true;
+        };
 }

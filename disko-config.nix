8755da0262c325fa79a1ee3e4908ffe682a6aa53
Boghul <82718422+Boghul@users.noreply.github.com>
2025-12-26 15:26:45 +0100

    Create disko-config.nix

---
+++ disko-config.nix
@@ -0,0 +1,43 @@
+# not ready!
+{ disks ? [ "/dev/vda" ], ... }: {
+  disko.devices = {
+    disk = {
+      vdb = {
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              start = "1MiB";
+              end = "500MiB";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "nofail"
+                                 "fmask=0077"
+                                 "dmask=0077"
+                                 ];
+              };
+            }
+            {
+              name = "root";
+              start = "500MiB";
+              end = "100%";
+              part-type = "primary";
+              content = {
+                type = "filesystem";
+                format = "bcachefs";
+                mountpoint = "/";
+              };
+            }
+          ];
+        };
+      };
+    };
+  };
+}

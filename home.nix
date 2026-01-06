e3aed3b982702d51318a4111241003369f15b847
Boghul <82718422+Boghul@users.noreply.github.com>
2026-01-06 17:11:47 +0100

    Update home.nix
    added gtk theme option

--- home.nix
+++ home.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   home = {
-    username = "xenomorph"; # Change to your username
+    username = "xenomorph";
     homeDirectory = "/home/xenomorph";
     stateVersion = "25.05";
     enableNixpkgsReleaseCheck = false;
@@ -20,9 +20,39 @@
     git = {
         enable = true;
         userName = "Boghul";
-        userEmail = "your.email@address";
+        userEmail = "thorsten.boeck@web.de";
          };
+
+ #   dconf.profiles.user.databases = [
+ # {
+ #   lockAll = true; # prevents overriding
+ #   settings = {
+ #     "org/gnome/desktop/interface" = {
+ #       color-scheme = "prefer-dark";
+ #     };
+ #   };
+ # }
+#];
+
     };
 
-    
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        #package = [pkgs.adwaita-dark];
+        };
+      iconTheme = {
+        name = "Adwaita-dark";
+        #package = [pkgs.gnome.adwaita-icon-theme];
+        };
+      cursorTheme = {
+        name = "Adwaita-dark";
+       # package = [pkgs.gnome.adwaita-icon-theme];
+        };
+    };
 }
+
+
+    
+
